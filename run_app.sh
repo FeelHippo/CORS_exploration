a0f3ccbb27e3d7f022b9cb3e1b94852aee5fd23a
Filippo Miorin <mezzasegolas@gmail.com>
2024-10-23 13:37:44 +0200

    add basic CORS request

--- run_app.sh
+++ run_app.sh
@@ -6,18 +6,25 @@ npm i -g pm2
 echo "Starting script: kill-ports and delete log file..."
 pm2 stop all
 
-echo "Starting server on port 3210..."
-pm2 --name Node start server.js -- start &
+echo "Starting server on port 3001..."
+echo "By default, the curl call will be made from port 3000..."
+NODE_PORT=3001 pm2 --name Node start server.js -- start &
 pm2 list
+pm2 save
 
 echo "Make a simple request..."
-curl --location 'localhost:3210/simple-request' \
+curl --location 'localhost:3001/simple-request' \
 --header 'Content-Type: multipart/form-data' \
 --form 'name="simple request"' \
 --form 'description="They don'\''t trigger a CORS preflight.  The motivation is that the <form> element from HTML 4.0 can submit simple requests to any origin. Under this assumption, the server doesn'\''t have to opt-in (by responding to a preflight request) to receive any request that looks like a form submission. However, the server still must opt-in using Access-Control-Allow-Origin to share the response with the script."' \
 --form 'accepted_methods="GET, HEAD, POST"' \
 --form 'accepted_manual_headers="Accept, Accept-Language, Content-Language, Content-Type, Range"' \
 --form 'accepted_type_subtype="application/x-www-form-urlencoded, multipart/form-data, text/plain"'
+
+echo "Make CORS request..."
+curl --location 'localhost:3001/basic-client-server' \
+--header 'Content-Type: multipart/form-data' \
+--header 'Origin: localhost:3000'
 
 echo "End of script: kill-port"
 pm2 stop all
--- server.js
+++ server.js
@@ -1,23 +1,64 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer(
     async (request, response) => {
         switch(request.method) {
+            case 'GET':
+                await getHandler(request, response);
+                break;
             case 'POST':
                 await postHandler(request, response);
                 break;
         }
     }
 ).listen(
-    3000, // Postman's default host is port 3000
+    process.env.NODE_PORT || 3001,
     console.info,
 );
 
+async function getHandler(request, response) {
+    const { url, rawHeaders, method } = request;
+    console.info(
+        `Incoming call:
+            ${method}
+            ${url}
+            ${rawHeaders}
+        `
+    );
+    try {
+        switch(url) {
+            case '/basic-client-server':
+                const xmlFile = fs.readFileSync('./note.xml');
+                response.writeHead(
+                    200,
+                    {
+                        'Access-Control-Allow-Origin': '*',
+                        'Keep-Alive': 'timeout=2, max=100',
+                        'Connection': 'Keep-Alive',
+                        'Content-Type': 'application/xml',
+                    }
+                );
+                response.end(
+                    xmlFile,
+                    'binary',
+                );
+                break;
+            default:
+                response.end();
+        }
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
 async function postHandler(request, response) {
-    const { url, rawHeaders } = request;
+    const { url, rawHeaders, method } = request;
     const body = await bodyParser(request);
     console.info(
         `Incoming call:
+            ${method}
             ${url}
             ${rawHeaders}
             ${body}
@@ -33,7 +74,6 @@ async function postHandler(request, response) {
                         'Keep-Alive': 'timeout=2, max=100',
                         'Connection': 'Keep-Alive',
                         'Content-Type': 'application/json',
-
                     }
                 );
                 response.end(
@@ -54,7 +94,7 @@ async function postHandler(request, response) {
 }
 
 async function bodyParser(request) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve, _reject) => {
     let body = '';
     request.on('data', data => body += data);
     request.on('end', () => resolve(body));
